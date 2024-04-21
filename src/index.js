96c82877ce4c5d22f26a37db16933ac3956dd9ad
hajerde <hajersoliman54@gmail.com>
2024-04-21 13:04:00 +0200

    updated return message

--- src/index.js
+++ src/index.js
@@ -32,13 +32,13 @@ mongoose
 
 app.get('/', (req, res) => {
   redisClient.set('products' , 'products...');
-  res.send(`<h1>HELLO TRESMERGE!!</h1>`);
+  res.send('<h1>Hello Tresmerge!</h1>');
 });
 
 app.get('/data', async (req, res) => {
     const products = await redisClient.get('products');
-    res.send(`<h1>HELLO TRESMERGE!</h1> <h2>${'products'}</h2>`);
+    res.send(`<h1>HELLO TRESMERGE!</h1> <h2>${products}</h2>`);
   });
 
-app.listen(PORT, () => console.log(`app is up and on port ${PORT}`));
+app.listen(PORT, () => console.log(`app is up and on port: ${PORT}`));
 
